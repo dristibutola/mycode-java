8841ac98a6ef9df44fc8fc5f5315c909e2da2782
dristybutola <drishtibutola@gmail.com>
2016-12-13 00:08:03 +0530

    added comments

--- java4.java
+++ java4.java
@@ -3,12 +3,11 @@ public class java4
 {
 public static void main(String[] args)
 {
+/**nextLine() and next()
+have the eof as \n and ' ' respectively**/
 
-
-
-
-
-
+/** use parsing to skip repeating nextLine()
+ when int or double is preceding it**/
 }
 }
 
