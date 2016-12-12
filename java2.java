ed389d19d47a9b9dc4f7fb2b5d87545741715b8e
dristybutola <drishtibutola@gmail.com>
2016-12-13 00:14:18 +0530

    simple scanner class use for integer

---
+++ java2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class java2
+{
+public static void main(String args[])
+{
+Scanner scan = new Scanner(System.in);
+int a = scan.nextInt();
+int b = scan.nextInt();
+int c= scan.nextInt();
+scan.close();   /*i can't find it in other examples usually :p*/
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+}
+}
